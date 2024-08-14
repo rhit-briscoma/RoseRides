574f927446c387addffce065ef3d9cd2241b0c3d
Howardnt <howardnt@rose-hulman.edu>
2024-08-14 13:22:18 -0400

    Javascript cleanup

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,31 +1,16 @@
 /**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
+ * @authors 
+ * Noah Howard
+ * Matt Brisco
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
 
-rhit.ClassName = class {
-	constructor() {
 
-	}
-
-	methodName() {
-
-	}
-}
 
 /* Main */
 /** function and class syntax examples */
