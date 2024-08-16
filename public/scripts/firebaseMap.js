4f1a93644ad2b766ab8e7c52f5c3d145c89e018e
Howardnt <howardnt@rose-hulman.edu>
2024-08-15 22:28:07 -0400

    Map partial functionality

--- public/scripts/firebaseMap.js
+++ public/scripts/firebaseMap.js
@@ -1,7 +1,95 @@
+var firebase = new Firebase("https://RoseRides.NAM5.firebasedatabase.app");
+
+
+/**
+ * Data object to be written to Firebase.
+ */
+var data = {
+    sender: null,
+    timestamp: null,
+    lat: null,
+    lng: null
+  };
+
+/**
+* Starting point for running the program. Authenticates the user.
+* @param {function()} onAuthSuccess - Called when authentication succeeds.
+*/
+function initAuthentication(onAuthSuccess) {
+    firebase.auth().signInAnonymously().catch(function(error) {
+        console.log(error.code + ", " + error.message);
+    }, {remember: 'sessionOnly'});
+  
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        data.sender = user.uid;
+        onAuthSuccess();
+      } else {
+        // User is signed out.
+      }
+    });
+  }
+
+  /**
+ * Set up a Firebase with deletion on clicks older than expirySeconds
+ * @param {!google.maps.visualization.HeatmapLayer} heatmap The heatmap to
+ * which points are added from Firebase.
+ */
+function initFirebase(heatmap) {
+
+    // 10 minutes before current time.
+    var startTime = new Date().getTime() - (60 * 10 * 1000);
+  
+    // Reference to the clicks in Firebase.
+    var clicks = firebase.database().ref('clicks');
+  
+    // Listen for clicks and add them to the heatmap.
+    clicks.orderByChild('timestamp').startAt(startTime).on('child_added',
+      function(snapshot) {
+        // Get that click from firebase.
+        var newPosition = snapshot.val();
+        var point = new google.maps.LatLng(newPosition.lat, newPosition.lng);
+        var elapsedMs = Date.now() - newPosition.timestamp;
+  
+        // Add the point to the heatmap.
+        heatmap.getData().push(point);
+  
+        // Request entries older than expiry time (10 minutes).
+        var expiryMs = Math.max(60 * 10 * 1000 - elapsed, 0);
+        // Set client timeout to remove the point after a certain time.
+        window.setTimeout(function() {
+          // Delete the old point from the database.
+          snapshot.ref.remove();
+        }, expiryMs);
+      }
+    );
+  
+    // Remove old data from the heatmap when a point is removed from firebase.
+    clicks.on('child_removed', function(snapshot, prevChildKey) {
+      var heatmapData = heatmap.getData();
+      var i = 0;
+      while (snapshot.val().lat != heatmapData.getAt(i).lat()
+        || snapshot.val().lng != heatmapData.getAt(i).lng()) {
+        i++;
+      }
+      heatmapData.removeAt(i);
+    });
+  }
+
+  // Listen for clicks and add them to the heatmap.
+clicks.orderByChild('timestamp').startAt(startTime).on('child_added',
+    function(snapshot) {
+      var newPosition = snapshot.val();
+      var point = new google.maps.LatLng(newPosition.lat, newPosition.lng);
+      heatmap.getData().push(point);
+    }
+  );
+
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.4833, lng: 87.3241},
-        zoom: 5,
+        center: {lat: 39.4833, lng: -87.3241},
+        zoom: 17,
         styles: [{
         featureType: 'poi',
         stylers: [{ visibility: 'off' }]  // Turn off points of interest.
